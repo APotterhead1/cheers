14edd4f2bbb036d1a0222be69042f87196d89ec1
Craig Foulkrod` <craig.foulkrod@gmail.com>
2026-06-09 00:19:49 -0400

    Updated build.gradle.kts and module-info.java

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    id( "java" )
+    id( "maven-publish")
 }
 
 group = "me.apotterhead"
@@ -17,4 +18,12 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+// Craig Foulkrod
+// 06082026-06082026
+
+module me.apotterhead.cheers {
+    exports me.apotterhead.cheers;
+}
